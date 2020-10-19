11b780163d0b78ae8825a47f1359767593d42895
PhillipHoribe <Horibe.phillip@globo.com>
2020-10-19 14:21:43 -0300

    first commit

---
+++ src/Testa.java
@@ -0,0 +1,14 @@
+
+public class Testa {
+	public static void main(String[]args) { 
+	Hora hora;
+		hora = new Hora();
+		System.out.println("hora: "+Hora.getHora());
+		System.out.println("Minutos: "+Hora.getSeg());
+		System.out.println("Segundos: "+Hora.getMin());
+		System.out.println("Horario: "+Hora.getHora1());
+		System.out.println("Horario: "+Hora.getHora2());
+		//System.out.println("Segundos totais: "+Hora.getSegundos());
+	}
+}
+
